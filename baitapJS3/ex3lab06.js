41f2d3c0c2fc7ae8fd748978729b2ef326bb787b
nhuanh9 <49300627+nhuanh9@users.noreply.github.com>
2019-05-30 08:39:40 +0700

    Add files via upload

--- baitapJS3/ex3lab06.js
+++ baitapJS3/ex3lab06.js
@@ -1,35 +1,29 @@
 var number = "";
 var pheptinh = "";
+var arr = [];
 function getValue(val1){
 	number = document.getElementById("number").value;
 	document.getElementById("number").value = number+""+val1;
 	number = val1;
+	arr.push(number);
 }
 function pheptoan(val2){
 	pheptinh = document.getElementById("number").value;
 	document.getElementById("number").value =pheptinh+ ""+val2;
 	pheptinh = val2;
+	arr.push(pheptinh);
 }
 function ketqua(){
 	ketquaTinh = 0;
-	switch(pheptinh){
-		case "+":
-		ketquaTinh = parseInt(number)+parseInt(document.getElementById("number").value);
-		break;
-		case "-":
-		ketquaTinh = parseInt(number)-parseInt(document.getElementById("number").value);
-		break;
-		case "*":
-		ketquaTinh = parseInt(number)*parseInt(document.getElementById("number").value);
-		break;
-		case "/":
-		ketquaTinh = parseInt(number)/parseInt(document.getElementById("number").value);
-		break;
+	for (var i =0;i<arr.length;i++){
+		ketquaTinh+= arr[i];
 	}
-		document.getElementById("number").value=ketquaTinh;
+	ketquaTinh=eval(ketquaTinh);
+	document.getElementById("number").value=ketquaTinh;
 }
 function reset(){
 	number = "";
 	pheptinh = "";
+	arr = [];
 	document.getElementById("number").value="";
 }
